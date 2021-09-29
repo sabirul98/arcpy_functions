06a90ccccf9775e381cb3fe668747789db722fda
sabirul98 <87126941+sabirul98@users.noreply.github.com>
2021-09-29 13:49:25 +0530

    Add files via upload

--- centroid.py
+++ centroid.py
@@ -1,3 +1,11 @@
+import arcpy
 
+arcpy.env.workspace ="D:\UNOM 2nd  Sem\Paper writting"
+
+input=("D:\UNOM 2nd  Sem\Paper writting\WB_Export_Output.shp")
+output="D:\UNOM 2nd  Sem\Paper writting\WB_Export_Output_centeroid1.shp"
+
+arcpy.FeatureToPoint_management(input,output,"CENTROID")
+print arcpy.GetMessages()
 
 
